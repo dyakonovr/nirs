5fe6fc0d5f0882e63d3bbe714cb93593141720d9
kirin <kirin3243@gmail.com>
2023-03-26 00:55:03 +0400

    dictionary done without links in text

--- mysite/apps/additionalStuff/models.py
+++ mysite/apps/additionalStuff/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from apps.articles.models import Topic
 
-# Create your models here.
+class Dictionary(models.Model):
+    topic = models.ForeignKey(Topic,on_delete=models.Model)
+    term = models.TextField()
+    definition = models.TextField()
--- mysite/apps/additionalStuff/views.py
+++ mysite/apps/additionalStuff/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Dictionary
 
 
 def dict(request):
-    return render(request, 'dict.html')
+    terms = Dictionary.objects.all()
+    content = {
+        'terms': terms,
+    }
+    return render(request, 'dict.html', context=content)
 
 
 def dates(request):

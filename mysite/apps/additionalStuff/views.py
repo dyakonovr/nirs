bc16bbb0036523f3b8fca382f4ff8dae97defae8
kirin <kirin3243@gmail.com>
2023-03-31 23:23:51 +0400

    first two topics dates are rdy

--- mysite/apps/additionalStuff/views.py
+++ mysite/apps/additionalStuff/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from .models import Dictionary
+from .models import Dictionary, Dates
 from apps.articles.models import Topic
 
 def dict(request):
     topicDict = {}
     for topic in Topic.objects.all():
-        topicDict[topic.topic] = Dictionary.objects.filter(topic=topic.id)
-    print(topicDict)
+        topicDict[topic.topic] = (Dictionary.objects.filter(topic=topic.id), Dates.objects.filter(topic=topic.id))
     content = {
         'topicDict': topicDict,
     }
